6e64b4024db5270cf8843e8068926dc91390c540
juanlopezcode <68015933+juanlopezcode@users.noreply.github.com>
2023-01-02 04:17:54 +0100

    Create utils.py

---
+++ src/baselines/utils/utils.py
@@ -0,0 +1,27 @@
+# Adapted from https://github.com/caogang/wgan-gp/blob/master/gan_toy.py
+def calc_gradient_penalty(net_dis, real_data, fake_data, batch_size, lmbda, use_cuda=False):
+    # Compute interpolation factors
+    alpha = torch.rand(batch_size, 1, 1)
+    alpha = alpha.expand(real_data.size())
+    alpha = alpha.cuda() if use_cuda else alpha
+
+    # Interpolate between real and fake data.
+    interpolates = alpha * real_data + (1 - alpha) * fake_data
+    if use_cuda:
+        interpolates = interpolates.cuda()
+    interpolates = autograd.Variable(interpolates, requires_grad=True)
+
+    # Evaluate discriminator
+    disc_interpolates = net_dis(interpolates)
+
+    # Obtain gradients of the discriminator with respect to the inputs
+    gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
+                              grad_outputs=torch.ones(disc_interpolates.size()).cuda() if use_cuda else
+                              torch.ones(disc_interpolates.size()),
+                              create_graph=True, retain_graph=True, only_inputs=True)[0]
+    gradients = gradients.view(gradients.size(0), -1)
+
+    # Compute MSE between 1.0 and the gradient of the norm penalty to make discriminator
+    # to be a 1-Lipschitz function.
+    gradient_penalty = lmbda * ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+    return gradient_penalty
