d82aa2e4a207cf2c3211dd4c41c34a5aec7f61db
juanlopezcode <68015933+juanlopezcode@users.noreply.github.com>
2023-02-17 00:07:29 +0100

    Update S4Model.py

--- src/sssd/models/S4Model.py
+++ src/sssd/models/S4Model.py
@@ -1008,7 +1008,6 @@ def get_torch_trans(heads=8, layers=1, channels=64):
 
 
 
-
 class S4(nn.Module):
 
     def __init__(
@@ -1063,9 +1062,123 @@ class S4(nn.Module):
         if self.hyper:
             channels *= 2
             self.hyper_activation = Activation(hyper_act)
-            
 
+        self.D = nn.Parameter(torch.randn(channels, self.h))
+
+        if self.bidirectional:
+            channels *= 2
+
+
+        # SSM Kernel
+        self.kernel = HippoSSKernel(self.h, N=self.n, L=l_max, channels=channels, verbose=verbose, **kernel_args)
+
+        # Pointwise
+        self.activation = Activation(activation)
+        dropout_fn = nn.Dropout2d if self.transposed else nn.Dropout
+        self.dropout = dropout_fn(dropout) if dropout > 0.0 else nn.Identity()
+
+        
+        # position-wise output transform to mix features
+        self.output_linear = LinearActivation(
+            self.h*self.channels,
+            self.h,
+            transposed=self.transposed,
+            initializer=initializer,
+            activation=postact,
+            activate=True,
+            weight_norm=weight_norm,
+        )
+        
+        #self.time_transformer = get_torch_trans(heads=8, layers=1, channels=self.h)
+        
+        
+        
+
+    def forward(self, u, **kwargs): # absorbs return_output and transformer src mask
+        """
+        u: (B H L) if self.transposed else (B L H)
+        state: (H N) never needed unless you know what you're doing
+
+        Returns: same shape as u
+        """
+        if not self.transposed: u = u.transpose(-1, -2)
+        L = u.size(-1)
+
+        # Compute SS Kernel
+        k = self.kernel(L=L) # (C H L) (B C H L)
+
+        # Convolution
+        if self.bidirectional:
+            k0, k1 = rearrange(k, '(s c) h l -> s c h l', s=2)
+            k = F.pad(k0, (0, L)) \
+                    + F.pad(k1.flip(-1), (L, 0)) \
+
+        k_f = torch.fft.rfft(k, n=2*L) # (C H L)
+        u_f = torch.fft.rfft(u, n=2*L) # (B H L)
+        y_f = contract('bhl,chl->bchl', u_f, k_f) # k_f.unsqueeze(-4) * u_f.unsqueeze(-3) # (B C H L)
+        y = torch.fft.irfft(y_f, n=2*L)[..., :L] # (B C H L)
+
+
+        # Compute D term in state space equation - essentially a skip connection
+        y = y + contract('bhl,ch->bchl', u, self.D) # u.unsqueeze(-3) * self.D.unsqueeze(-1)
+
+        # Optional hyper-network multiplication
+        if self.hyper:
+            y, yh = rearrange(y, 'b (s c) h l -> s b c h l', s=2)
+            y = self.hyper_activation(yh) * y
+
+        # Reshape to flatten channels
+        y = rearrange(y, '... c h l -> ... (c h) l')
+
+        y = self.dropout(self.activation(y))
+
+        if not self.transposed: y = y.transpose(-1, -2)
+
+        y = self.output_linear(y)
+
+        # ysize = b, k, l, requieres l, b, k 
+        #y = self.time_transformer(y.permute(2,0,1)).permute(1,2,0)
             
+            
+        return y, None
+        
+
+    def step(self, u, state):
+        """ Step one time step as a recurrent model. Intended to be used during validation.
+
+        u: (B H)
+        state: (B H N)
+        Returns: output (B H), state (B H N)
+        """
+        assert not self.training
+
+        y, next_state = self.kernel.step(u, state) # (B C H)
+        y = y + u.unsqueeze(-2) * self.D
+        y = rearrange(y, '... c h -> ... (c h)')
+        y = self.activation(y)
+        if self.transposed:
+            y = self.output_linear(y.unsqueeze(-1)).squeeze(-1)
+        else:
+            y = self.output_linear(y)
+        return y, next_state
+
+    def default_state(self, *batch_shape, device=None):
+        return self.kernel.default_state(*batch_shape)
+
+    @property
+    def d_state(self):
+        return self.h * self.n
+
+    @property
+    def d_output(self):
+        return self.h
+
+    @property
+    def state_to_tensor(self):
+        return lambda state: rearrange('... h n -> ... (h n)', state)
+
+
+    
 class S4Layer(nn.Module):
     #S4 Layer that can be used as a drop-in replacement for a TransformerEncoder
     def __init__(self, features, lmax, N=64, dropout=0.0, bidirectional=True, layer_norm=True):
